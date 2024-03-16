8c043a16eebebf063bb34b383ccdfea8e98f540f
Chris Trivino <trivinochris124@gmail.com>
2024-03-16 16:07:00 -0600

    1. Added sections for document Outline

--- ui.R
+++ ui.R
@@ -1,7 +1,6 @@
-
 source("./tabs/DML_Tab/dml_ui.R")
 
-
+#### Nav Bar UI ####
 navbarPage(id = "tabs", "Causal Analysis Demo", dml_outcome)
 # colors: bs_theme(bg ='#2222', fg ='#F3F6F8',,success = '#00BC8C'  ) 
 
