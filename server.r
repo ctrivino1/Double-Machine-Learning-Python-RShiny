76ed867d2d56abda1ea9389a839a7aaf242925aa
Chris Trivino <trivinochris124@gmail.com>
2024-03-16 16:06:41 -0600

    1. Added sections for document outline 2. added a dummy row to show a user on how to add another tab server file

--- server.r
+++ server.r
@@ -1,4 +1,7 @@
-
 source("./tabs/DML_Tab/dml_server.R")
+
+#### Render different Tab Server Files ####
 shinyServer(function(input, output, session) {
-  render_dml_tab(input, output, session)})
+  render_dml_tab(input, output, session)
+  #another_tab_name(input, output, session) # make sure to source the other server file above
+  })
