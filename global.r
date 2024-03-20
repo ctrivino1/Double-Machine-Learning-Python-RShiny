c67b6f243e798d24a5a7c9383630450d120712c4
Chris Trivino <trivinochris124@gmail.com>
2024-03-20 12:44:04 -0600

    deleted file

--- global.r
+++ global.r
@@ -14,6 +14,7 @@ library(tidyverse)
 library(openxlsx)
 library(lubridate)
 library(glue)
+library(plotly)
 
 
 # logger output to make sure packes load
@@ -26,13 +27,37 @@ global_dat <- read_csv("401k_practice data.csv")
 
 #### Global reactive values ####
 global <- reactiveValues(
+  # plots 
   ATE_summary = NULL,
   plr_summary = NULL,
+  # other 
   non_sig_treats = NULL,
   trigger = NULL,
+  # ATE DT actions
   hover_info = NULL,
   click_info = NULL,
-  dblclick_info = NULL)
+  dblclick_info = NULL,
+  # Exploration Graph counts
+  binary_count = 0,
+  continuous_count = 0,
+  string_count = 0,
+  selected_x_vars = NULL)
 
+#### Global Functions ####
+# Identify columns that contain strings or numeric values of 0 and 1
+string_or_binary_cols <- sapply(global_dat, function(x) is.character(x) || (is.numeric(x) && all(unique(x) %in% c(0, 1))))
 
+# Convert identified columns to factor columns
+global_dat[string_or_binary_cols] <- lapply(global_dat[string_or_binary_cols], as.factor)
 
+# Function to classify variable types
+get_variable_type <- function(variable) {
+  if ((is.factor(variable) && all(levels(variable) %in% c("0", "1"))) ||  # Check if factor with levels 0 and 1
+      (is.numeric(variable) && all(variable %in% c(0, 1)))) {              # Check if numeric with values 0 and 1
+    "binary"                                                               # Return "binary" if the conditions are met
+  } else if (is.factor(variable)) {                                       
+    "string"                                                                # Return "string" if the variable is a factor but not binary
+  } else {
+    "continuous"                                                            # Return "continuous" if the variable is numeric but not binary
+  }
+}
