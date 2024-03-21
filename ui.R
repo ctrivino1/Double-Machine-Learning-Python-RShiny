9c2159e6ef5914b952cd17a7379187b3608655ba
Chris Trivino <trivinochris124@gmail.com>
2024-03-20 18:34:56 -0600

    added my github link to title bar, and shaded it green.

--- ui.R
+++ ui.R
@@ -1,7 +1,21 @@
-source("./tabs/DML_Tab/dml_ui.R")
+source('./tabs/DML_Tab/dml_ui.R')
+JS_navbar <- tags$script(HTML(
+  "var header = $('.navbar > .container-fluid');
+   var githubURL = 'https://github.com/ctrivino1/Double-Machine-Learning-Python-RShiny.git'; 
+   header.append('<div style=\"float:right;margin-right: 10px;\"><a href=\"' + githubURL + '\"><img src=\"github_logo.png\" alt=\"GitHub\" style=\"width:75px;height:50px;padding-top:2px;\"></a></div>');
+   header.append('<div style=\"float:right;margin-right: 10px;padding-top:20px;\"><a href=\"' + githubURL + '\">Follow me on GitHub</a></div>');"
+))
+
+css_navbar <- tags$style(HTML('
+    .navbar-default {
+        background-color: #28b78d !important;
+    }
+
+    
+'))
 
 #### Nav Bar UI ####
-navbarPage(id = "tabs", "Causal Analysis Demo", dml_outcome)
+navbarPage(id = 'tabs', 'Causal Analysis Demo', dml_outcome,JS_navbar,css_navbar)
 # colors: bs_theme(bg ='#2222', fg ='#F3F6F8',,success = '#00BC8C'  ) 
 
 
