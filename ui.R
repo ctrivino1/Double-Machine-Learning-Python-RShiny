16264c5836a923e87a69af8fa8b5646923fa5ac8
Chris Trivino <trivinochris124@gmail.com>
2024-03-22 10:05:31 -0600

    added code for logo and github link text to show up

--- ui.R
+++ ui.R
@@ -1,28 +1,33 @@
 source('./tabs/DML_Tab/dml_ui.R')
 JS_navbar <- tags$script(HTML(
-  "
-   / This puts the logo and text link to my github repo
-   var header = $('.navbar > .container-fluid');
-   var githubURL = 'https://github.com/ctrivino1/Double-Machine-Learning-Python-RShiny.git'; 
-   header.append('<div style=\"float:right;margin-right: 10px;\"><a href=\"' + githubURL + '\"><img src=\"github_logo.png\" alt=\"GitHub\" style=\"width:75px;height:50px;padding-top:2px;\"></a></div>'); 	
-   header.append('<div style=\"float:right;margin-right: 10px;padding-top:20px;\"><a href=\"' + githubURL + '\">Follow me on GitHub</a></div>');"
+  '
+  // Add GitHub logo and text link to the navbar
+  $(document).ready(function() {
+    var header = $(".navbar > .container-fluid");
+    var githubURL = "https://github.com/ctrivino1/Double-Machine-Learning-Python-RShiny.git"; 
+    header.append(\'<div style="float:right;margin-right: 10px;"><a href="\' + githubURL + \'"><img src="github_logo.png" alt="GitHub" style="width:75px;height:50px;padding-top:2px;"></a></div>\');
+    header.append(\'<div style="float:right;margin-right: 10px;padding-top:20px;"><a href="\' + githubURL + \'" style="color: white;">Follow me on GitHub</a></div>\');
+  });
+  '
 ))
 
-css_navbar <- tags$style(HTML("
-    /* this makes the navBar a specific color */
-    .navbar-default {
-        background-color: #28b78d !important;
-    }
-    
-    /* This makes the tab background whie */
-    .navbar-default .navbar-nav > .active > a,
-    .navbar-default .navbar-nav > .active > a:focus,
-    .navbar-default .navbar-nav > .active > a:hover {
-        color: #555 !important; /* Set text color for active tabs */
-        background-color: white !important; /* Set background color for active tabs */
-    }
+css_navbar <- tags$style(HTML(
+  '
+  /* Set background color of navbar */
+  .navbar-default {
+    background-color: #28b78d !important;
+  }
+  
+  /* Set styling for active tabs */
+  .navbar-default .navbar-nav > .active > a,
+  .navbar-default .navbar-nav > .active > a:focus,
+  .navbar-default .navbar-nav > .active > a:hover {
+    color: #555 !important; /* Text color for active tabs */
+    background-color: white !important; /* Background color for active tabs */
+  }
+  '
+))
 
-"))
 
 #### Nav Bar UI ####
 navbarPage(id = 'tabs', span( 'Causal Analysis App', style = 'background-color: ##28b78d; color: white'), dml_outcome,JS_navbar,css_navbar)
