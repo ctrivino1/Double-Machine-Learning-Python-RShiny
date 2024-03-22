964a77c0fd236abf33746f8c1de252dab9dcfa62
Chris Trivino <trivinochris124@gmail.com>
2024-03-22 10:39:59 -0600

    added ability to access github in new window

--- ui.R
+++ ui.R
@@ -1,15 +1,28 @@
 source('./tabs/DML_Tab/dml_ui.R')
+# JS_navbar <- tags$script(HTML(
+#   '
+#   // Add GitHub logo and text link to the navbar
+#   $(document).ready(function() {
+#     var header = $(".navbar > .container-fluid");
+#     var githubURL = "https://github.com/ctrivino1/Double-Machine-Learning-Python-RShiny.git"; 
+#     header.append(\'<div style="float:right;margin-right: 10px;"><a href="\' + githubURL + \'"><img src="github_logo.png" alt="GitHub" style="width:75px;height:50px;padding-top:2px;"></a></div>\');
+#     header.append(\'<div style="float:right;margin-right: 10px;padding-top:20px;"><a href="\' + githubURL + \'" style="color: white;">Follow me on GitHub</a></div>\');
+#   });
+#   '
+# ))
+
 JS_navbar <- tags$script(HTML(
   '
   // Add GitHub logo and text link to the navbar
   $(document).ready(function() {
     var header = $(".navbar > .container-fluid");
     var githubURL = "https://github.com/ctrivino1/Double-Machine-Learning-Python-RShiny.git"; 
-    header.append(\'<div style="float:right;margin-right: 10px;"><a href="\' + githubURL + \'"><img src="github_logo.png" alt="GitHub" style="width:75px;height:50px;padding-top:2px;"></a></div>\');
-    header.append(\'<div style="float:right;margin-right: 10px;padding-top:20px;"><a href="\' + githubURL + \'" style="color: white;">Follow me on GitHub</a></div>\');
+    header.append(\'<div style="float:right;margin-right: 10px;padding-top:20px;"><a href="\' + githubURL + \'" target="_blank" style="color: white; text-decoration: underline;">Follow me on GitHub</a></div>\');
+    header.append(\'<div style="float:right;margin-right: 10px;"><a href="\' + githubURL + \'" target="_blank"><img src="github_logo.png" alt="GitHub" style="width:75px;height:50px;padding-top:2px;"></a></div>\');
   });
   '
 ))
+
 
 css_navbar <- tags$style(HTML(
   '
