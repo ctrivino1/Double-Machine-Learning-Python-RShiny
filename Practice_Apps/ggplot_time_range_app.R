40247b58ccad322e8282114d94b5237f5b0864c3
Chris Trivino <trivinochris124@gmail.com>
2024-04-10 10:05:37 -0600

    updated app

--- Practice_Apps/ggplot_time_range_app.R
+++ Practice_Apps/ggplot_time_range_app.R
@@ -21,7 +21,8 @@ ui <- fluidPage(
         label = "Select range of years:", range = T,
         #value = c(min(gapminder$year),max(gapminder$year))
         minDate = c(min(gapminder$year)), maxDate = c(max(gapminder$year))
-      )
+      ),
+      checkboxInput("all_years", "Include All Years", value = FALSE)
     ),
     mainPanel(
       plotlyOutput("gapminder_plot")
@@ -31,13 +32,26 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   output$gapminder_plot <- renderPlotly({
-    yr_input <<- input$range
-    filtered_data <- gapminder %>%
-      filter(year >= input$range[1] & year <= input$range[2])
+    # Check if input$range is not null and has length 2, or if input$all_years is not null
+    req(length(input$range) == 2 || !is.null(input$all_years))
+    
+    if (length(input$range) == 2) {
+      filtered_data <- gapminder %>%
+        filter(year >= input$range[1] & year <= input$range[2])
+      frame_value <- filtered_data$year
+    } else if (input$all_years) {
+      filtered_data <- gapminder
+      frame_value <- filtered_data$year
+    } else {
+      # If conditions are not met, return NULL to prevent rendering
+      return(NULL)
+    }
     
     g <- highlight_key(filtered_data, ~filtered_data[[input$continent]])
-    gg <- ggplot(g, aes(gdpPercap, lifeExp, 
-                        color = filtered_data[[input$continent]], frame = year)) +
+    
+    # Use regular if-else syntax within aes_string
+    gg <- ggplot(g, aes_string('gdpPercap', 'lifeExp', 
+                               color = filtered_data[[input$continent]], frame = frame_value)) +
       geom_point() +
       stat_smooth(se = FALSE, method = "lm") +
       scale_x_log10()
@@ -45,8 +59,7 @@ server <- function(input, output) {
     p <- highlight(ggplotly(gg), "plotly_hover")
     p
   })
-  
-  
 }
+
 
 shinyApp(ui, server)
